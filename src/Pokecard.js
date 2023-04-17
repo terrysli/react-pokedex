29697920f4152931717035ef3854062eea8e124a
William Squires <william.squiresii@gmail.com>
2023-04-17 17:45:37 -0500

    added styling for pokeCards. moved some destructuring around.

--- src/Pokecard.js
+++ src/Pokecard.js
@@ -1,16 +1,21 @@
+import './Pokecard.css'
+
 const BASE_URL = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon";;
 const myStyle = {listStyleType: "none"};
+
 
 /**
  * Show name, image, and type of single Pokemon
  */
-function Pokecard({ id, name, type }) {
+function Pokecard({pokemon}) {
+  const {name, id, type, base_experience} = pokemon;
   return (
-    <ul style={myStyle}>
-      <li>{name}</li>
-      <li><img src={`${BASE_URL}/${id}.png`} /></li>
-      <li>{type}</li>
-    </ul>
+    <div className='pokemonContainer'>
+      <p><b>{name}</b></p>
+      <img src={`${BASE_URL}/${id}.png`} />
+      <p>Type: {type}</p>
+      <p>Exp: {base_experience}</p>
+    </div>
   );
 }
 
--- src/Pokedex.js
+++ src/Pokedex.js
@@ -4,8 +4,8 @@ import Pokecard from "./Pokecard";
  * Show sequence of Pokecard components based on array of Pokemon data
  */
 function Pokedex(props) {
-  return props.pokemon.map(({ id, name, type }) =>
-    <Pokecard id={id} name={name} type={type} />);
+  return props.pokemon.map(pokemon =>
+    <Pokecard pokemon={pokemon} />);
 }
 
 export default Pokedex;
